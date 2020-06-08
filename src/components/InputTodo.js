8e8e6014536688207a4dc6aea9f9fb5910b51302
Gheorghe Tarcea <tarcea@gmail.com>
2020-06-08 20:15:35 +0200

    trials

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -7,14 +7,29 @@ class InputTodo extends Component {
 
   onChange = e => {
     this.setState({
-      title: e.target.value
+      // title: e.target.value
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addTodoProps(this.state.title);
+    this.setState({
+      title: ""
     });
   };
 
   render() {
     return (
-      <form>
-        <input type="text" placeholder="Add Todo...." value={this.state.title} onChange={this.onChange} />
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add Todo...."
+          value={this.state.title}
+          name="title"
+          onChange={this.onChange}
+        />
         <input type="submit" value="Submit" />
       </form>
       )
